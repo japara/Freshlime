0de43f9cd6893d2b2a57319bc6bf1260ee960e27
japara <72559893+japara@users.noreply.github.com>
2024-10-25 13:40:08 +0400

    header

---
+++ freshlime/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../images/logo.png";
+
+function Header() {
+  return (
+    <div className="flex justify-center px-[4.8rem] bg-[#e6f2ef]">
+      <nav className="flex justify-between items-center w-[100%] h-[96px] ">
+        <img src={logo} alt="logo" width={118}></img>
+        <ul className="list-none flex items-center gap-[2.8rem] text-[18px] font-[500] ">
+          <li className="cursor-pointer">How it works</li>
+          <li className="cursor-pointer">Inside us</li>
+          <li className="cursor-pointer">Testimonials</li>
+          <li className="cursor-pointer">Pricing</li>
+          <li className="cursor-pointer">
+            <button className="bg-[#087f5b] text-white  rounded-[9px] w-[144px] h-[42px]">
+              Try for free
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
