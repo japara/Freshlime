fe592355ffb866a39c214c0157a998a337d7a86a
japara <72559893+japara@users.noreply.github.com>
2024-10-27 22:34:11 +0400

    pricing

--- freshlime/src/components/Pricing.js
+++ freshlime/src/components/Pricing.js
@@ -3,6 +3,8 @@ import {
   IoSpeedometerOutline,
   IoCloseOutline,
   IoPulseOutline,
+  IoPause,
+  IoRocketOutline,
 } from "react-icons/io5";
 
 function Pricing() {
@@ -48,15 +50,15 @@ function Pricing() {
             </div>
           </div>
         </div>
-        <div className="flex relative overflow-hidden gap-[80px] h-fit py-[3rem] justify-center items-center mx-[80px] mb-[6rem] bg-[#e6f2ef] border-[2px] border-[#e6f2ef] rounded-[11px] ">
+        <div className="flex relative overflow-hidden gap-[80px] px-[13rem] h-fit py-[3rem] justify-center items-center mx-[80px] mb-[3rem] bg-[#e6f2ef] rounded-[11px] ">
           <div className="flex-1 flex flex-col items-center px-20">
             <div className="text-center">
               <p className="font-[600] text-[#077252] text-[1.6rem]">Monthly</p>
               <p className="font-[900]  text-[1.6rem] mt-[0.8rem]">
-                $ <span className="text-[3.6rem]">45</span>
+                $ <span className="text-[3.6rem]">600</span>
               </p>
               <p className="font-[600] text-[#777] text-[1.2rem] mt-[0.8rem]">
-                Pay every single month
+                Per month it's just 30$
               </p>
               <ul className="text-[18px] w-[15rem] ">
                 <li className="flex justify-left items-center gap-3 mt-[2rem]">
@@ -88,18 +90,56 @@ function Pricing() {
           </div>
         </div>
       </div>
-      <p className="mb-[3rem]">
+      <p className="mb-[5rem]">
         Prices include all applicable taxes. You can cancel at any time. Both
         plans include the following:
       </p>
-
-      <div className="text-left">
-        <div className="rounded-full bg-[#b5d9ce] w-fit p-4">
-          {" "}
-          <IoPulseOutline className="text-[#087f5b] h-[26px] w-[26px] " />
+      <div className="flex justify-center gap-10 mt-[1rem] px-6">
+        <div className="text-left flex-1">
+          <div className="rounded-full bg-[#b5d9ce] w-fit p-4">
+            {" "}
+            <IoPulseOutline className="text-[#087f5b] h-[26px] w-[26px] " />
+          </div>
+          <p className="font-[800] text-[22px] mt-[1.8rem]">
+            Tranin every day!
+          </p>
+          <p className="mt-[1rem] leading-7">
+            Our gym is open 365 days in year, even holidays you can come!
+          </p>
         </div>
-        <p>Tranin every day!</p>
-        <p>Our gym is open 365 days in year, even holidays you can come!</p>
+        <div className="text-left flex-1">
+          <div className="rounded-full bg-[#b5d9ce] w-fit p-4">
+            {" "}
+            <IoRocketOutline className="text-[#087f5b] h-[26px] w-[26px] " />
+          </div>
+          <p className="font-[800] text-[22px] mt-[1.8rem]">
+            Natural and organic
+          </p>
+          <p className="mt-[1rem] leading-7">
+            Our proteins are best in the world and organic good for health
+          </p>
+        </div>
+        <div className="text-left flex-1">
+          <div className="rounded-full bg-[#b5d9ce] w-fit p-4">
+            {" "}
+            <IoRocketOutline className="text-[#087f5b] h-[26px] w-[26px] " />
+          </div>
+          <p className="font-[800] text-[22px] mt-[1.8rem]">Motivation</p>
+          <p className="mt-[1rem] leading-7">
+            We have motivation speakers to motivate you every day
+          </p>
+        </div>
+        <div className="text-left flex-1">
+          <div className="rounded-full bg-[#b5d9ce] w-fit p-4">
+            {" "}
+            <IoPause className="text-[#087f5b] h-[26px] w-[26px] " />
+          </div>
+          <p className="font-[800] text-[22px] mt-[1.8rem]">Pause anytime</p>
+          <p className="mt-[1rem] leading-7">
+            You got full year and have vacation? pause it, it is possible with
+            us
+          </p>
+        </div>
       </div>
     </div>
   );
